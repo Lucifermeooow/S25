acad6d32cd8e4156c90478ce916e936f94932853
Lucifermeooow <impossible7man@gmail.com>
2026-09-08 01:03:27 +0000

    fix(android): force compileSdkVersion 35 and buildToolsVersion 35.0.0 on library subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.3")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
-
         maven {
             url = java.net.URI("https://jitpack.io")
         }
@@ -11,6 +20,22 @@ allprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    project.plugins.withId("com.android.library") {
+        project.extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+            compileSdkVersion(35)
+            buildToolsVersion("35.0.0")
+        }
+    }
+    project.afterEvaluate {
+        val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        android?.apply {
+            compileSdkVersion(35)
+            buildToolsVersion("35.0.0")
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
